3956f19eed13bd9ae1886374282f277ce77ac290
yangmu <deeymmm@gmail.com>
2025-04-05 18:02:00 +0800

    feat: add WebSocket examples and update dependencies

--- ccxt/examples.py
+++ ccxt/examples.py
@@ -1,8 +1,10 @@
+import asyncio
+import json
 import logging
 import os
 
-import ccxt
-import json
+import ccxt.async_support as ccxt
+import ccxt.pro as ccxtpro  # 使用ccxt pro
 
 # 确保日志目录存在
 log_dir = './log'
@@ -18,7 +20,7 @@ logging.basicConfig(
     ]
 )
 
-if __name__ == '__main__':
+def test_http():
     proxy_url = 'http://127.0.0.1:7897'
     proxy_settings = {
         'http': proxy_url,
@@ -31,3 +33,28 @@ if __name__ == '__main__':
     order_book = exchange.fetch_order_book('BTC/USDT', 3)
     # 用漂亮的json格式打印
     print(json.dumps(order_book, indent=4))
+
+async def example_web_sockets():
+    my_ex = ccxtpro.gate({
+        'options': {
+            'verify': False  # 禁用SSL验证
+        }
+    })
+    my_ex.http_proxy = 'http://127.0.0.1:7897/'
+    my_ex.ws_proxy = 'http://127.0.0.1:7897/'
+    await my_ex.load_markets()
+    while True:
+        ticker = await my_ex.watch_ticker('BTC/USDT')
+        print(ticker)
+
+async def test_ws():
+    exchange = ccxtpro.kraken({'newUpdates': False})
+    exchange.http_proxy = 'http://127.0.0.1:7897/'
+    exchange.ws_proxy = 'http://127.0.0.1:7897/'
+    while True:
+        orderbook = await exchange.watch_order_book('BTC/USD')
+        print(orderbook['asks'][0], orderbook['bids'][0])
+
+
+if __name__ == '__main__':
+    asyncio.run(example_web_sockets())
